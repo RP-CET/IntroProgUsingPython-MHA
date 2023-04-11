d751f84502aab9bc2cc8f168706de77a0f8e2dc5
Jason Lim <jason_lim@rp.edu.sg>
2023-04-11 13:08:52 +0800

    Delete install.py

--- install.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "pillow"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "beautifulsoup4"])
